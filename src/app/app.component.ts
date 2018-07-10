dd700696845a44a446568fe9d0dfda63e19cc233
Mário Brandão <mariobrandaobr@hotmail.com>
2018-07-09 22:39:38 -0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('myModal') myModal;
+  imgSrc = '';
+  modalTitle = '';
+  
+  ngOnInit() {
+  }
+  
+  openModal(event: any) {
+    this.modalTitle = event.path[0].alt;
+    this.imgSrc = event.path[0].currentSrc;
+    this.myModal.nativeElement.className = 'modal open';
+  };
+
+  closeModal() {
+    this.myModal.nativeElement.className = 'modal';
+  };
 }
